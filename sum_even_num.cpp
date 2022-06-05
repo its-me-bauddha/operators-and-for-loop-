c3df9b0d020b7fe664a773de8ce6c30f7a0819a2
Harsh bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2022-06-05 21:26:08 +0530

    Create sum_even_num.cpp

---
+++ sum_even_num.cpp
@@ -0,0 +1,30 @@
+/*Sum of Even Numbers till N
+Send Feedback
+Given a number N, print sum of all even numbers from 1 to N.
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+Sample Input 1 :
+ 6
+Sample Output 1 :*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n ;
+  //  cout << "Enter the number" << endl;
+    cin >> n;
+    
+    int i = 0;
+    int sum=0;
+        while ( i <=n ) {
+           
+               sum=sum+i;
+            i = i + 2;
+        }
+    cout<<sum;
+}
